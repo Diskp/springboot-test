40046f139af7991dff064a4af214e69d8d2ef378
hide <981392570@qq.com>
2020-11-19 23:02:47 +0800

    3

--- src/main/java/com/example/controller/Test.java
+++ src/main/java/com/example/controller/Test.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Test {
 	@RequestMapping("/test")
 	public String index() {
-		return "USA BAD2";
+		return "USA BAD3";
 	}
 }
